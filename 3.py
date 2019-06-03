fdbe227198fe89f52b57c4a6b0816ea49fc40f65
RaghulHari <51219524+RaghulHari@users.noreply.github.com>
2019-06-03 12:49:21 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+y=input()
+if(y=='a'and y==''and y=='i' and y=='o' and y=='u' and y=='A' and y=='E' and y=='I' and y=='O' and y=='U'):
+  print("Vowel")
+elif(y=='$'):
+  print("invalid")
+else:
+  print("Consonant")
