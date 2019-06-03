1759d2f3e627c4bc711de89b3d453ecb0f0559a8
RaghulHari <51219524+RaghulHari@users.noreply.github.com>
2019-06-03 12:51:19 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 y=input()
-if(y=='a'and y==''and y=='i' and y=='o' and y=='u' and y=='A' and y=='E' and y=='I' and y=='O' and y=='U'):
+if(y=='a'and y=='e'and y=='i' and y=='o' and y=='u' and y=='A' and y=='E' and y=='I' and y=='O' and y=='U'):
   print("Vowel")
 elif(y=='$'):
   print("invalid")
