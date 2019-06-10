f6910fce2c7e3de7476ee7e5e1a08ed685954f56
RaghulHari <51219524+RaghulHari@users.noreply.github.com>
2019-06-10 14:45:34 +0530

    Create 67.py

---
+++ 67.py
@@ -0,0 +1,9 @@
+oo=int(input())
+if(oo>=0 and oo<=10):
+    print("10")
+elif(oo>=11 and oo<=20):
+    print("20")
+elif(oo>=21 and oo<=30):
+    print("30")
+elif(oo>=31 and oo<=40):
+    print("40")
