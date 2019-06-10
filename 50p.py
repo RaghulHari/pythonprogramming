6a0837f2548001c0c7acbf9339b6225a20427ebe
RaghulHari <51219524+RaghulHari@users.noreply.github.com>
2019-06-10 15:17:26 +0530

    Rename 50.py to 50p.py

---
+++ 50p.py
@@ -0,0 +1,13 @@
+kk=int(input())
+z=x=0
+for i in range(1,kk):
+    x=0
+    p=2**i
+    if s==p:
+        print("yes")
+        exit()
+        x=1
+    else:
+        z=1
+if(x!=1):
+    print("no")
