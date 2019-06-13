d8a025af8c35879f1f533028a0e0b9bb16b96849
RaghulHari <51219524+RaghulHari@users.noreply.github.com>
2019-06-13 11:28:57 +0530

    Create 39p.py

---
+++ 39p.py
@@ -0,0 +1,23 @@
+import math 
+m=int(input())  
+
+def Log2(x): 
+    if x == 0: 
+        return false; 
+  
+    return (math.log10(x) / 
+            math.log10(2)); 
+  
+def isPowerOfTwo(n): 
+    return (math.ceil(Log2(n)) == 
+            math.floor(Log2(n))); 
+  
+
+if(isPowerOfTwo(m)): 
+    print("Yes"); 
+else: 
+    print("No"); 
+  
+ 
+      
+
