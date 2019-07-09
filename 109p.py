da7d90cb19c8190dfd332d14e0be1ec280e2c90f
RaghulHari <51219524+RaghulHari@users.noreply.github.com>
2019-07-09 18:15:01 +0530

    Create 109p.py

---
+++ 109p.py
@@ -0,0 +1,14 @@
+n=int(input())
+d=input()
+d=d.split()
+l=[]
+for i in d:
+  l.append(i)
+m=[]
+m.append(l[0])
+c=int(l[0])
+for i in range(0,n-1):
+  c=c+int(l[i+1])
+  m.append(c)
+d=m[::-1]
+print(*d,sep=" ")
